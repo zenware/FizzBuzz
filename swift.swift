1dfc2bd3546454325a3cd70bd0d9a5909f54245c
Rhed Shi <shi.rhed@gmail.com>
2015-10-13 22:33:20 -0700

    Added Swift

---
+++ swift.swift
@@ -0,0 +1,18 @@
+func FizzBuzz(num: Int) -> () {
+    if num % 15 == 0 {
+        print("FizzBuzz")
+    }
+    else if num % 5 == 0 {
+        print("Buzz")
+    }
+    else if num % 3 == 0 {
+        print("Fizz")
+    }
+    else {
+        print(num)
+    }
+}
+
+for i in 1...100 {
+    FizzBuzz(i)
+}
