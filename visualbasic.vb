bc17ba77ef64fae95864ea490040a84e4a8fb27e
YC <YC@users.noreply.github.com>
2016-01-27 13:30:09 +1100

    Add Visual Basic

---
+++ visualbasic.vb
@@ -0,0 +1,24 @@
+﻿Module FizzBuzz
+
+    Function FizzBuzz(num As Integer) As String
+        Dim Fizz As Boolean = (num Mod 3 = 0)
+        Dim Buzz As Boolean = (num Mod 5 = 0)
+
+        If Fizz = True And Buzz = True Then
+            Return "FizzBuzz"
+        ElseIf Fizz = True Then
+            Return "Fizz"
+        ElseIf Buzz = True Then
+            Return "Buzz"
+        End If
+
+        Return num.ToString
+    End Function
+    Sub Main()
+        For i = 1 To 100
+            Console.WriteLine(FizzBuzz(i))
+        Next
+        Console.Read()
+    End Sub
+
+End Module
