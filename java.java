93361166a074c93bb913f8e7d5593af09a752194
Noah Lovelady-Allen <noah+github@loveladyallen.com>
2016-02-10 17:52:35 -0500

    make java.java work
    It had defined fizzBuzz, but never called the method.

--- java.java
+++ java.java
@@ -3,7 +3,9 @@ public class java
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        for (int i=1; i<=100; i++) {
+            fizzBuzz(i);
+        }
     }
 
     public static void fizzBuzz(int num)
