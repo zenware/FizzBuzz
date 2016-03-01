22fb768ef9ddbf9a306cca2d6502b24613f4ea97
Gen Kamita <gk298@cam.ac.uk>
2016-03-01 22:21:02 +0000

    added matlab.m

---
+++ matlab.m
@@ -0,0 +1,14 @@
+for inum = 1:100
+  fizzbuzz = '';
+  if mod(inum,3) == 0
+	fizzbuzz = [fizzbuzz 'Fizz'];
+  end
+  if mod(inum,5) == 0
+	fizzbuzz = [fizzbuzz 'Buzz'];
+  end
+  if isempty(fizzbuzz)
+	disp(inum)
+  else
+	disp(fizzbuzz)
+  end
+end
