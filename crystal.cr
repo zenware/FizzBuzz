36eab073a4b6a4006bfad1b857132d440ff8e5d4
Jay Looney <jay.m.looney@gmail.com>
2016-05-20 12:27:26 -0500

    Symlink Crystal to Ruby

--- crystal.cr
+++ crystal.cr
@@ -1,15 +1,1 @@
-def fizz_buzz(num)
-  if num % 15 == 0
-    puts "FizzBuzz"
-  elsif num % 3 == 0
-    puts "Fizz"
-  elsif num % 5 == 0
-    puts "Buzz"
-  else
-    puts num
-  end
-end
-
-(1..100).each do |x|
-  fizz_buzz(x)
-end
+ruby.rb
