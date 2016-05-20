351825b4e1a42f6b24a0707eaaada720f0ce57d0
Noah Lovelady-Allen <noah+github@loveladyallen.com>
2016-05-20 09:39:06 -0400

    Use double quotes in ruby.rb

--- ruby.rb
+++ ruby.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
 def fizz_buzz(num)
-  result = ''
-  result += 'Fizz' if num % 3 == 0
+  result = ""
+  result += "Fizz" if num % 3 == 0
   result += "Buzz" if num % 5 == 0
   puts(result.empty? ? num : result)
 end
