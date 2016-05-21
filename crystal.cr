e89ba2659fb15c6cf204dc5bcc142a60146d1e9b
Chayoung You <yousbe@gmail.com>
2016-05-21 21:56:46 +0900

    Make Crystal implementation receive integer only

--- crystal.cr
+++ crystal.cr
@@ -1,1 +1,15 @@
-ruby.rb
+def fizz_buzz(num : Int)
+  if num % 15 == 0
+    puts "FizzBuzz"
+  elsif num % 3 == 0
+    puts "Fizz"
+  elsif num % 5 == 0
+    puts "Buzz"
+  else
+    puts num
+  end
+end
+
+(1..100).each do |x|
+  fizz_buzz(x)
+end
