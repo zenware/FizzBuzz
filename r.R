521c5c5209e858d3ad50a55c5c0ce9b98841f32c
Yoda <ravkalia@gmail.com>
2016-05-23 08:36:39 +0800

    Update r.R
    minor improvement

--- r.R
+++ r.R
@@ -1,10 +1,8 @@
-
- fizz_buzz <- function(num){
+fizz_buzz <- function(num){
   result <- ''
   if(!(num %% 3))   result <- 'Fizz' 
   if(!(num %% 5))  result <- paste0(result,'Buzz') 
-  ans <- ifelse(result=='', num, result)
-  print(ans)
+  print(ifelse(result=='', num, result))
 }
 
 sapply(seq_len(100), fizz_buzz)
