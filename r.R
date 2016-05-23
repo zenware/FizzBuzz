ce003bad62d4d1124b2ca762e97068b843ac0ef6
Yoda <ravkalia@gmail.com>
2016-05-23 15:14:21 +0800

    Update r.R
    print one array item per line

--- r.R
+++ r.R
@@ -1,6 +1,6 @@
 fizz_buzz <- function(num_array=1:100){
-result_array <-
-paste0(ifelse(num_array%%3L,'','Fizz'),ifelse(num_array%%5L,'','Buzz'))
+result_array <-paste0(ifelse(num_array%%3L,'','Fizz'),
+                      ifelse(num_array%%5L,'','Buzz'))
 result_array[result_array==''] <- num_array[result_array=='']
 done <- sapply(result_array, function(res) cat(paste0(res,'\n')))
 }
