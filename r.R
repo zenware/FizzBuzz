23403bb3071b3c1dd71959811a0ba9690209b1aa
Yoda <ravkalia@gmail.com>
2016-05-23 11:07:50 +0800

    Update r.R

--- r.R
+++ r.R
@@ -1,8 +1,8 @@
 fizz_buzz <- function(num){
   result <- ''
-  if(!(num %% 3L))   result <- 'Fizz' 
-  if(!(num %% 5L))  result <- paste0(result,'Buzz') 
-  print(ifelse(result=='',num, result))
+  if(!(num%%3L)) result <- 'Fizz' 
+  if(!(num%%5L)) result <- paste0(result,'Buzz') 
+  print(ifelse(result=='',num,result))
 }
 
-sapply(seq_len(100L), fizz_buzz)
+sapply(seq_len(100L),fizz_buzz)
