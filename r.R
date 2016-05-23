77c993e996cacf72323d40d0635a8f9f30f449dc
Yoda <ravkalia@gmail.com>
2016-05-23 15:36:03 +0800

    Update r.R
    using higher order function

--- r.R
+++ r.R
@@ -1,8 +1,8 @@
-fizz_buzz <- function(num_array=1:100){
-result_array <-paste0(ifelse(num_array%%3L,'','Fizz'),
-                      ifelse(num_array%%5L,'','Buzz'))
-result_array[result_array==''] <- num_array[result_array=='']
-done <- sapply(result_array, function(res) cat(paste0(res,'\n')))
+fizz_buzz <- function(num_array=seq_len(100L)){
+result_array <- Map(function(res,num){cat(paste0(ifelse(res=='',num,res),'\n'))},
+                    res=paste0(ifelse(num_array%%3L,'','Fizz'),
+                               ifelse(num_array%%5L,'','Buzz')),
+                    num=num_array)
 }
 
 fizz_buzz()
