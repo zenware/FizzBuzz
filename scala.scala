c0796784be36d4ca936fec56c63d3b72634b63a9
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2016-06-03 10:56:59 -0300

    Update scala.scala
    Modified to use Streams.

--- scala.scala
+++ scala.scala
@@ -1,20 +1,11 @@
-object Scala {
-
-  def main(args: Array[String]): Unit = {
-    for (i <- 1 to 100) {
-      fizzBuzz(i);
-    }
+object Scala extends App {
+  
+  val fizzBuzz = Stream.from(1).map {
+    case n if n % 15 == 0 => "FizzBuzz"
+    case n if n % 5  == 0 => "Buzz"
+    case n if n % 3  == 0 => "Fizz"
+    case n                => n
   }
-
-  def fizzBuzz(num: Int): Unit = {
-    if (num % 15 == 0) {
-      println("FizzBuzz");
-    } else if (num % 5 == 0) {
-      println("Buzz");
-    } else if (num % 3 == 0) {
-      println("Fizz");
-    } else {
-      println(num);
-    }
-  }
+  
+  println(fizzBuzz.take(100).mkString("\n"))
 }
