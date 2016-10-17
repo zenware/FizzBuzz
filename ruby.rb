c8552ff45272ace97f851438f693fd221de51545
nloveladyallen <noah+github@loveladyallen.com>
2016-10-17 06:51:50 -0400

    Improve Ruby style

--- ruby.rb
+++ ruby.rb
@@ -1,12 +1,10 @@
 #!/usr/bin/env ruby
 
 def fizz_buzz(num)
-  result = ""
-  result += "Fizz" if num % 3 == 0
-  result += "Buzz" if num % 5 == 0
-  puts(result.empty? ? num : result)
+  result = ''
+  result += 'Fizz' if (num % 3).zero?
+  result += 'Buzz' if (num % 5).zero?
+  puts result.empty? ? num : result
 end
 
-(1..100).each{|x|
-  fizz_buzz(x)
-}
+(1..100).each { |x| fizz_buzz x }
