75bf1c7ce7ed11b30cb9f10ee50d0fdd7e12e8d1
Nicholas <nickcfeldman@gmail.com>
2017-02-07 11:17:02 -0500

    Don't display leading zeroes

--- cobol.cbl
+++ cobol.cbl
@@ -5,6 +5,8 @@
        01 CT PIC 999 VALUE 1.
        01 FZ PIC 999 VALUE 1.
        01 BZ PIC 999 VALUE 1.
+       01 RESULT-STRING PIC x(3).
+       01 SPACE-COUNT PIC 99 VALUE ZERO.
        PROCEDURE DIVISION.
        FIZZ-BUZZ-MAIN SECTION.
            PERFORM 100 TIMES
@@ -17,13 +19,20 @@
                            COMPUTE FZ = 0
                         ELSE IF BZ = 5
                         THEN DISPLAY "Buzz"
-                                COMPUTE BZ = 0
+                           COMPUTE BZ = 0
                         ELSE
-                                DISPLAY CT
+                            MOVE 0 TO SPACE-COUNT
+                            INSPECT CT TALLYING SPACE-COUNT
+                                FOR LEADING ZEROES
+                            MOVE CT
+                                (SPACE-COUNT + 1 :
+                                    LENGTH OF CT - SPACE-COUNT)
+                                        TO RESULT-STRING
+                            DISPLAY RESULT-STRING
                         END-IF
                 END-IF
                 ADD 1 TO CT
                 ADD 1 TO FZ
                 ADD 1 TO BZ
            END-PERFORM
-         STOP RUN.
+        STOP RUN.
