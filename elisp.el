979e919016d2f04e4ebd7813cd62b4e730bbca72
mohamed-aziz <medazizknani@gmail.com>
2018-01-28 20:57:51 +0100

    Fix issues with Emacs implementation

--- elisp.el
+++ elisp.el
@@ -1,13 +1,11 @@
 :;exec emacs -Q --script "$0" "$@"
 
 (defun fizzbuzz (n)
-  (let  ((v (if (> n 0)
-		(fizzbuzz (1- n))
-	      "")))
-    (concat v "\n" (pcase `(,(% n 3) ,(% n 5))
-		     (`(0 0) "FizzBuzz")
-		     (`(0 ,_) "Fizz")
-		     (`(,_ 0) "Buzz")
-		     (`(,_ ,_) (number-to-string n))))))
+  (append (if (> n 1) (fizzbuzz (1- n)))
+          (list (pcase `(,(% n 3) ,(% n 5))
+              (`(0 0) "FizzBuzz")
+              (`(0 ,_) "Fizz")
+              (`(,_ 0) "Buzz")
+              (`(,_ ,_) (number-to-string n))))))
 
-(princ (fizzbuzz 100))
+(princ (mapconcat 'identity (mapcar (lambda (x) (concat x "\n")) (fizzbuzz 100)) ""))
