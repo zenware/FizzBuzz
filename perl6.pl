0e3d9b069982c8fdb2ab33931f870b4e341813ec
Hydrazer <explodingaerosal@gmail.com>
2021-06-05 09:58:05 -0600

    Update perl6.pl

--- perl6.pl
+++ perl6.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/env perl6
 for 1 .. 100 {
-	if ($_ % 15 == 0) {
+	if ($_ %% 15) {
 		say "FizzBuzz";
-	} elsif ($_ % 3 == 0) {
+	} elsif ($_ %% 3) {
 		say "Fizz";
-	} elsif ($_ % 5 == 0) {
+	} elsif ($_ %% 5) {
 		say "Buzz";
 	} else {
 		say $_;
